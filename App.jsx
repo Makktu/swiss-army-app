92c95e7090dfaf0e0e66d6db6c55aeac91c0ff07
Makktu <johnmcna@gmail.com>
2023-12-27 20:17:19 +0000

    getting set up

--- App.jsx
+++ App.jsx
@@ -1,19 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Welcome from './src/screens/Welcome';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style='light' />
+      <NavigationContainer>
+        <Welcome />
+      </NavigationContainer>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1e1c1c',
     alignItems: 'center',
     justifyContent: 'center',
   },
