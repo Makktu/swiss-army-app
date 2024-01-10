a1f3be6c9fd581367c36a2c403c365a1abafed22
Makktu <johnmcna@gmail.com>
2024-01-10 21:00:51 +0000

    added all sounds and working on UI

--- src/components/MyButton.jsx
+++ src/components/MyButton.jsx
@@ -1,32 +1,37 @@
 import { StyleSheet, Text, View, Pressable } from 'react-native';
-import React from 'react';
-import { GlobalStyles } from '../admin/globalStyles';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function MyButton({
+export default function IconButton({
+  icon,
+  size,
+  color = 'white',
   whenPressed,
   whenLongPressed,
-  whenSwitchReleased,
-  style,
-  children,
+  whenReleased,
 }) {
   return (
-    <View style={[{ borderRadius: 10 }, style]}>
-      <Pressable
-        onPress={whenPressed}
-        onLongPress={whenLongPressed}
-        onPressOut={whenSwitchReleased}
-        style={({ pressed }) => pressed && styles.pressed}
-      >
-        <Text style={style}>{children}</Text>
-      </Pressable>
-    </View>
+    <Pressable
+      onPress={whenPressed}
+      onLongPress={whenLongPressed}
+      onPressOut={whenReleased}
+      style={({ pressed }) => pressed && styles.pressed}
+    >
+      <View>
+        <Ionicons name={icon} size={size} color={color} />
+      </View>
+    </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 12,
+  },
+  icon: {
+    fontSize: 36,
+  },
   pressed: {
-    opacity: 0.75,
-    backgroundColor: GlobalStyles.colors.primary100,
-    borderRadius: 4,
+    opacity: 0.5,
+    color: 'gray',
   },
 });
--- src/screens/PressureAlarmScreen.jsx
+++ src/screens/PressureAlarmScreen.jsx
@@ -1,39 +1,80 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React, { useState } from 'react';
-// import Sound from 'react-native-sound';
+import React, { useState, useEffect } from 'react';
+import { Audio } from 'expo-av';
 import MyButton from '../components/MyButton';
 import CheckScreenDimensions from '../components/CheckScreenDimensions';
 
-// Sound.setCategory('Playback');
-
 export default function PressureAlarmScreen() {
   const [switchPressed, setSwitchPressed] = useState(false);
+  const [sound, setSound] = useState();
+
+  async function playBeep() {
+    console.log('Loading sound...');
+    const { sound } = await Audio.Sound.createAsync(
+      require(`./../../assets/sounds/beep.wav`)
+    );
+
+    setSound(sound);
+    console.log('playing sound...');
+    await sound.playAsync();
+  }
+  async function playAlarm() {
+    const { sound } = await Audio.Sound.createAsync(
+      require(`./../../assets/sounds/alarm.wav`)
+    );
+    setSound(sound);
+    console.log('playing sound...');
+    await sound.playAsync();
+  }
+
   const buttonPressHandler = () => {
     console.log('pressed');
   };
   const longButtonPressHandler = () => {
     console.log('pressed and held');
+    playBeep();
     setSwitchPressed(true);
   };
-  const switchReleasedHandler = () => {
+  const releasedHandler = () => {
     if (switchPressed) {
       console.log('alarm!!!');
+      playAlarm();
       setSwitchPressed(false);
     }
   };
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          console.log('Unloading Sound');
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
   return (
     <View style={switchPressed ? styles.stealthContainer : styles.container}>
+      <View style={styles.messageArea}>
+        {switchPressed ? (
+          <Text style={styles.armedText}>ARMED !</Text>
+        ) : (
+          <>
+            <Text style={styles.unArmedText}>Press AND HOLD</Text>
+            <Text style={styles.unArmedText}>this button to ARM</Text>
+          </>
+        )}
+      </View>
       <MyButton
+        icon='power-outline'
+        size={300}
+        color='red'
         whenPressed={buttonPressHandler}
         whenLongPressed={longButtonPressHandler}
-        whenSwitchReleased={switchReleasedHandler}
+        whenReleased={releasedHandler}
         style={
           switchPressed ? styles.stealthPressureSwitch : styles.pressureSwitch
         }
-      >
-        PRESS
-      </MyButton>
-      <CheckScreenDimensions />
+      />
+      {/* <CheckScreenDimensions /> */}
     </View>
   );
 }
@@ -51,8 +92,8 @@ const styles = StyleSheet.create({
     // borderRadius: 100,
   },
   stealthPressureSwitch: {
-    width: 200,
-    height: 200,
+    // width: 200,
+    // height: 200,
     backgroundColor: '#131513',
     // borderRadius: 100,
   },
@@ -61,5 +102,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: 'black',
+  },
+  armedText: {
+    fontSize: 40,
+    color: 'orangered',
+  },
+  unArmedText: {
+    fontSize: 30,
+    color: 'white',
+  },
+  messageArea: {
+    marginBottom: 50,
+    textAlign: 'center',
+    alignItems: 'center',
   },
 });
